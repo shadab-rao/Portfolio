c91f3d30e1700e4aa7b61e3244726ff2f66ea0b1
Shadab Rao <shdb790073@gmail.com>
2026-07-20 16:08:34 +0530

    Project api

--- portfolio-backend/src/routes/education.route.js
+++ portfolio-backend/src/routes/education.route.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const educationController = require("../controllers/education.controller");
 router.post("/create",educationController.createEducation);
 router.get("/all",educationController.getAllEducation);
-router.get("/experience/:id",educationController.educationById);
+router.get("/details/:id",educationController.educationById);
 router.put("/update/:id",educationController.updateEducation);
 router.delete("/delete/:id",educationController.deleteEducation);
 
