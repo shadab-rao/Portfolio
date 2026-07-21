c945a2d41b31049a23d237ef44fa72dd6a4cd205
Shadab Rao <shdb790073@gmail.com>
2026-07-21 12:23:54 +0530

    fix keys

--- portfolio-backend/src/config/cloudinary.js
+++ portfolio-backend/src/config/cloudinary.js
@@ -3,6 +3,8 @@ const cloudinary = require("cloudinary").v2;
 console.log("Cloud name:", process.env.CLOUD_NAME);
 
 console.log("Cloud key:", process.env.CLOUD_KEY);
+console.log("Secret key:", process.env.JWT_SECRET);
+console.log("Port:", process.env.PORT);
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
