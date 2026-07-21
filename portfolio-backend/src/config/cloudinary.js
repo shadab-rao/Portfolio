d9229c1df6beb92caa30938c4a01292734fc779e
Shadab Rao <shdb790073@gmail.com>
2026-07-21 12:12:40 +0530

    test and fix 433

--- portfolio-backend/src/config/cloudinary.js
+++ portfolio-backend/src/config/cloudinary.js
@@ -1,5 +1,8 @@
 const cloudinary = require("cloudinary").v2;
 
+console.log("Cloud name:", process.env.CLOUD_NAME);
+console.log("Cloud key:", process.env.CLOUD_KEY);
+
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_KEY,
