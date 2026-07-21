dfadc6dbc9f5055746110a5fe0621c03ce772c73
Shadab Rao <shdb790073@gmail.com>
2026-07-21 15:35:21 +0530

    nodemailer

--- portfolio-backend/src/utils/sendMail.js
+++ portfolio-backend/src/utils/sendMail.js
@@ -2,12 +2,19 @@ const nodemailer = require("nodemailer");
 
 const sendMail = async ({ name, email, message, subject }) => {
   try {
+
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+
+      connectionTimeout: 10000,
+      socketTimeout: 10000,
     });
 
 
@@ -15,38 +22,24 @@ const sendMail = async ({ name, email, message, subject }) => {
       from: process.env.EMAIL_USER,
       to: process.env.EMAIL_USER,
 
-      // Email subject
       subject: `Portfolio Contact: ${subject}`,
 
       html: `
-        <h3>New Portfolio Contact Message</h3>
+        <h3>New Portfolio Contact</h3>
 
-        <p>
-          <b>Subject:</b> ${subject}
-        </p>
+        <p><b>Subject:</b> ${subject}</p>
+        <p><b>Name:</b> ${name}</p>
+        <p><b>Email:</b> ${email}</p>
 
-        <p>
-          <b>Name:</b> ${name}
-        </p>
-
-        <p>
-          <b>Email:</b> ${email}
-        </p>
-
-        <p>
-          <b>Message:</b>
-        </p>
-
-        <p>
-          ${message}
-        </p>
+        <p><b>Message:</b></p>
+        <p>${message}</p>
       `,
     });
 
 
     console.log("Mail sent successfully");
 
-  } catch (error) {
+  } catch(error){
     console.log(error);
     throw error;
   }
