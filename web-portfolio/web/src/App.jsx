d76bbdd81340578fea4cae996ee033b34c0c3427
Shadab Rao <shdb790073@gmail.com>
2026-07-21 14:07:28 +0530

    deploy web

--- web-portfolio/web/src/App.jsx
+++ web-portfolio/web/src/App.jsx
@@ -3,7 +3,7 @@ import Contact from "./components/contact/Contact";
 import Education from "./components/education/Education";
 import Experience from "./components/experience/Experience";
 import Footer from "./components/footer/Footer";
-import Hero from "./components/hero/hero";
+import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Projects from "./components/projects/Projects";
 import Skills from "./components/skills/Skills";
